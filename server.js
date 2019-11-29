dfc69bfddcbec904783726a59a8a676e7e2c8b32
Danilo Cardoso <dsc.soares@gmail.com>
2019-11-28 23:01:46 -0300

    trabalhando com rotas

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const requireDir = require('require-dir')
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Hello!')
+//iniciar o db
+mongoose.connect('mongodb://localhost:27017/nodeapi', { 
+    useNewUrlParser : true, 
+    useUnifiedTopology: true 
 })
+requireDir('./src/models')
+
+//Rotas
+app.use('/api', require('./src/routes'))
 app.listen(3001)
